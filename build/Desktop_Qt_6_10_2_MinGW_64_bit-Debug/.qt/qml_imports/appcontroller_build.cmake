172f0f2bb7796e2858e8ef695b932bae06882e10
Matthew Furkert <matthew@furkert.com>
2026-02-13 19:02:37 -0500

    improved rotation

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/qml_imports/appcontroller_build.cmake
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/qml_imports/appcontroller_build.cmake
@@ -1,4 +1,4 @@
-set(qml_import_scanner_imports_count 12)
+set(qml_import_scanner_imports_count 13)
 set(qml_import_scanner_import_0 "CLASSNAME;QtQuick2Plugin;LINKTARGET;Qt6::qtquick2plugin;NAME;QtQuick;PATH;C:/Qt/6.10.2/mingw_64/qml/QtQuick;PLUGIN;qtquick2plugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/;RELATIVEPATH;QtQuick;TYPE;module;")
 set(qml_import_scanner_import_1 "CLASSNAME;QtQmlPlugin;LINKTARGET;Qt6::qmlplugin;NAME;QtQml;PATH;C:/Qt/6.10.2/mingw_64/qml/QtQml;PLUGIN;qmlplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/;RELATIVEPATH;QtQml;TYPE;module;")
 set(qml_import_scanner_import_2 "NAME;QML;PATH;C:/Qt/6.10.2/mingw_64/qml/QML;PREFER;:/qt-project.org/imports/QML/;RELATIVEPATH;QML;TYPE;module;")
@@ -11,4 +11,5 @@ set(qml_import_scanner_import_8 "CLASSNAME;QtQuickControls2BasicStyleImplPlugin;
 set(qml_import_scanner_import_9 "CLASSNAME;QmlShapesPlugin;LINKTARGET;Qt6::qmlshapesplugin;NAME;QtQuick.Shapes;PATH;C:/Qt/6.10.2/mingw_64/qml/QtQuick/Shapes;PLUGIN;qmlshapesplugin;PREFER;:/qt-project.org/imports/QtQuick/Shapes/;RELATIVEPATH;QtQuick/Shapes;TYPE;module;")
 set(qml_import_scanner_import_10 "CLASSNAME;QtQmlLabsModelsPlugin;LINKTARGET;Qt6::labsmodelsplugin;NAME;Qt.labs.qmlmodels;PATH;C:/Qt/6.10.2/mingw_64/qml/Qt/labs/qmlmodels;PLUGIN;labsmodelsplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/Qt/labs/qmlmodels/;RELATIVEPATH;Qt/labs/qmlmodels;TYPE;module;")
 set(qml_import_scanner_import_11 "CLASSNAME;QtQuickLayoutsPlugin;LINKTARGET;Qt6::qquicklayoutsplugin;NAME;QtQuick.Layouts;PATH;C:/Qt/6.10.2/mingw_64/qml/QtQuick/Layouts;PLUGIN;qquicklayoutsplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/Layouts/;RELATIVEPATH;QtQuick/Layouts;TYPE;module;")
+set(qml_import_scanner_import_12 "CLASSNAME;QtQuick_WindowPlugin;LINKTARGET;Qt6::quickwindow;NAME;QtQuick.Window;PATH;C:/Qt/6.10.2/mingw_64/qml/QtQuick/Window;PLUGIN;quickwindowplugin;PREFER;:/qt-project.org/imports/QtQuick/Window/;RELATIVEPATH;QtQuick/Window;TYPE;module;")
 
--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/controller/Main.qml
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/controller/Main.qml
@@ -1,24 +1,30 @@
 import QtQuick
 import QtQuick.Controls.Basic
 import QtQuick.Layouts
+import QtQuick.Window
 
 ApplicationWindow {
+    readonly property bool embeddedDev: Qt.platform.os !== "windows" && Qt.platform.os !== "osx"
+
     id: window
-    width: 800
-    height: 480
+    width: embeddedDev ? 480 : 800
+    height: embeddedDev ? 800 : 480
     visible: true
     title: qsTr("Alpha Control Interface")
 
     FontLoader {
-            id: prismaFont
-            source: Qt.resolvedUrl("fonts/Prisma.ttf")
-        }
+        id: prismaFont
+        source: Qt.resolvedUrl("fonts/Prisma.ttf")
+    }
 
     Page {
-        padding: 10
-        anchors.fill: parent
-        font.family: prismaFont.name
+        anchors.centerIn: parent
+        width: embeddedDev ? window.height : window.width
+        height: embeddedDev ? window.width : window.height
+        rotation: embeddedDev ? 90 : 0
 
+        padding: 10
+        font.family: prismaFont.name
         background: Rectangle {
             color: "red"
         }
@@ -33,6 +39,7 @@ ApplicationWindow {
                 Layout.fillHeight: true
                 Layout.preferredWidth: 100
             }
+
             Workspace {
                 id: zone2
                 Layout.fillHeight: true
